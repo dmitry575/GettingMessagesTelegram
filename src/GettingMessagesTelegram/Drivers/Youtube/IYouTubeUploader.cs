9984ba7c7f0f6775b6cd7548231b1223e4b087e1
Dmitry Tarakanov <mail.dimm@gmail.com>
2022-10-30 22:44:51 +0300

    upload video api

---
+++ src/GettingMessagesTelegram/Drivers/Youtube/IYouTubeUploader.cs
@@ -0,0 +1,16 @@
+﻿
+namespace GettingMessagesTelegram.Drivers.Youtube
+{
+    /// <summary>
+    /// Upload video to youtube
+    /// </summary>
+    public interface IYouTubeUploader
+    {
+        /// <summary>
+        /// Upload one file to youtube
+        /// </summary>
+        /// <param name="fileName">Full file path</param>
+        /// <param name="cancellation"></param>
+        Task<bool> UploadAsync(string fileName, CancellationToken cancellation = default);
+    }
+}
---
+++ src/GettingMessagesTelegram/Drivers/Youtube/Impl/YouTubeUploader.cs
@@ -0,0 +1,51 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3.Data;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+
+namespace GettingMessagesTelegram.Drivers.Youtube.Impl
+{
+    public class YouTubeUploader:IYouTubeUploader
+    {
+        public Task<bool> UploadAsync(string fileName, CancellationToken cancellation = default)
+        {
+            var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+               new ,
+                // This OAuth 2.0 access scope allows an application to upload files to the
+                // authenticated user's YouTube channel, but doesn't allow other types of access.
+                new[] { YouTubeService.Scope.YoutubeUpload },
+                "user",
+                CancellationToken.None
+            );
+
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = Assembly.GetExecutingAssembly().GetName().Name
+            });
+
+            var video = new Video();
+            video.Snippet = new VideoSnippet();
+            video.Snippet.Title = "Default Video Title";
+            video.Snippet.Description = "Default Video Description";
+            video.Snippet.Tags = new string[] { "tag1", "tag2" };
+            video.Snippet.CategoryId = "22"; // See https://developers.google.com/youtube/v3/docs/videoCategories/list
+            video.Status = new VideoStatus();
+            video.Status.PrivacyStatus = "unlisted"; // or "private" or "public"
+            var filePath = @"REPLACE_ME.mp4"; // Replace with path to actual movie file.
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var videosInsertRequest = youtubeService.Videos.Insert(video, "snippet,status", fileStream, "video/*");
+               
+                await videosInsertRequest.UploadAsync();
+            }
+        }
+    }
+}
